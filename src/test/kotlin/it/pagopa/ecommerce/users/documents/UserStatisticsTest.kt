bbec229e008ce2e1e22c8da44b339f2616f498a4
scaminati-bv <simone.caminati@bvtech.com>
2024-10-03 18:15:06 +0200

    fix: add missing test for guest type

--- src/test/kotlin/it/pagopa/ecommerce/users/documents/UserStatisticsTest.kt
+++ src/test/kotlin/it/pagopa/ecommerce/users/documents/UserStatisticsTest.kt
@@ -10,12 +10,17 @@ class UserStatisticsTest {
     fun `can build UserStatistics document`() {
         val walletType = UserTestUtils.userStatisticsByType(LastUsage.PaymentType.WALLET)
         val guestType = UserTestUtils.userStatisticsByType(LastUsage.PaymentType.GUEST)
-        assertNotNull(guestType)
         assertNotNull(walletType)
         assertNotNull(walletType.userId)
         assertNotNull(walletType.lastUsage)
         assertNotNull(walletType.lastUsage.type)
         assertNotNull(walletType.lastUsage.date)
         assertNotNull(walletType.lastUsage.instrumentId)
+        assertNotNull(guestType)
+        assertNotNull(guestType.userId)
+        assertNotNull(guestType.lastUsage)
+        assertNotNull(guestType.lastUsage.type)
+        assertNotNull(guestType.lastUsage.date)
+        assertNotNull(guestType.lastUsage.instrumentId)
     }
 }
