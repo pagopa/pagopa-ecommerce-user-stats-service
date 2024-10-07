081fc2ffa62abd5f513014529da1023277ae2680
Pietro Tota <115724836+pietro-tota@users.noreply.github.com>
2024-10-07 16:10:59 +0200

    feat: add warmup requests (#11)
    * feat: add warmup requests
    
    * fix: increase warmup function timeout
    
    * fix: spotless

--- src/main/kotlin/it/pagopa/ecommerce/users/controllers/v1/LastUsageController.kt
+++ src/main/kotlin/it/pagopa/ecommerce/users/controllers/v1/LastUsageController.kt
@@ -1,19 +1,25 @@
 package it.pagopa.ecommerce.users.controllers.v1
 
 import it.pagopa.ecommerce.users.services.UserStatisticsService
+import it.pagopa.ecommerce.users.warmup.annotations.WarmupFunction
+import it.pagopa.ecommerce.users.warmup.utils.WarmupUtils
 import it.pagopa.generated.ecommerce.users.api.UserApi
 import it.pagopa.generated.ecommerce.users.model.UserLastPaymentMethodData
+import java.time.Duration
 import java.util.*
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.reactive.function.client.WebClient
 import org.springframework.web.server.ServerWebExchange
 import reactor.core.publisher.Mono
 
 @RestController("LastUsageControllerV1")
-class LastUsageController(@Autowired private val userStatisticsService: UserStatisticsService) :
-    UserApi {
+class LastUsageController(
+    @Autowired private val userStatisticsService: UserStatisticsService,
+    private val webClient: WebClient = WebClient.create(),
+) : UserApi {
 
     override fun getLastPaymentMethodUsed(
         xUserId: UUID,
@@ -43,4 +49,25 @@ class LastUsageController(@Autowired private val userStatisticsService: UserStat
                 .saveUserLastUsedMethodInfo(userId = xUserId, userLastPaymentMethodData = it)
                 .map { ResponseEntity.status(HttpStatus.CREATED).build() }
         }
+
+    @WarmupFunction
+    fun saveAndGetUserLastPaymentMethodUsedWarmupFunction() {
+        webClient
+            .post()
+            .uri(WarmupUtils.LAST_PAYMENT_METHOD_USED_PATH)
+            .bodyValue(WarmupUtils.warmupLastMethodUsedBody)
+            .header(WarmupUtils.X_USER_ID_HEADER_KEY, WarmupUtils.zeroUUID.toString())
+            .retrieve()
+            .toBodilessEntity()
+            .flatMap {
+                // perform GET right after performing warmup POST request
+                webClient
+                    .get()
+                    .uri(WarmupUtils.LAST_PAYMENT_METHOD_USED_PATH)
+                    .header(WarmupUtils.X_USER_ID_HEADER_KEY, WarmupUtils.zeroUUID.toString())
+                    .retrieve()
+                    .toBodilessEntity()
+            }
+            .block(Duration.ofSeconds(30))
+    }
 }
---
+++ src/main/kotlin/it/pagopa/ecommerce/users/warmup/utils/WarmupUtils.kt
@@ -0,0 +1,16 @@
+package it.pagopa.ecommerce.users.warmup.utils
+
+import it.pagopa.generated.ecommerce.users.model.WalletLastUsageData
+import java.time.OffsetDateTime
+import java.util.*
+
+object WarmupUtils {
+    const val LAST_PAYMENT_METHOD_USED_PATH = "http://localhost:8080/user/lastPaymentMethodUsed"
+    const val X_USER_ID_HEADER_KEY = "x-user-id"
+    val zeroUUID: UUID = UUID.fromString("00000000-0000-0000-0000-000000000000")
+    val warmupLastMethodUsedBody =
+        WalletLastUsageData().apply {
+            this.walletId = zeroUUID
+            this.date = OffsetDateTime.now()
+        }
+}
