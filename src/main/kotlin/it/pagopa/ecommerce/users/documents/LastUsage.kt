df19d6efce9ac768eb7a21bebc973b39257a2347
Pietro Tota <pietro.tota@pagopa.it>
2024-10-09 14:42:11 +0200

    fux: add k-doc

--- src/main/kotlin/it/pagopa/ecommerce/users/documents/LastUsage.kt
+++ src/main/kotlin/it/pagopa/ecommerce/users/documents/LastUsage.kt
@@ -3,12 +3,16 @@ package it.pagopa.ecommerce.users.documents
 import java.time.OffsetDateTime
 import java.util.*
 
+/** Common interface for last usage method details */
 sealed interface LastUsageMethodDetails
 
+/** Details for wallet method */
 data class WalletLastUsageMethodDetails(val walletId: UUID) : LastUsageMethodDetails
 
+/** Details for guest payment method */
 data class GuestLastUsageMethodDetails(val paymentMethodId: UUID) : LastUsageMethodDetails
 
+/** Data class containing last usage information */
 data class LastUsage(
     val details: LastUsageMethodDetails,
     /** The date of the last usage */
