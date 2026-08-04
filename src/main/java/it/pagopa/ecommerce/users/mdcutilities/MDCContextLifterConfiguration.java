f8127d18c32283448074cae02851358c7d353880
Stefano Rondinella <stefrondinella@live.it>
2026-08-04 11:53:07 +0200

    feat(logging): add utilies classes for logging

---
+++ src/main/java/it/pagopa/ecommerce/users/mdcutilities/MDCContextLifterConfiguration.java
@@ -0,0 +1,41 @@
+package it.pagopa.ecommerce.users.mdcutilities;
+
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
+import org.springframework.context.annotation.Configuration;
+import reactor.core.publisher.Hooks;
+import reactor.core.publisher.Operators;
+
+/**
+ * Registers Reactor hooks to propagate MDC values across reactive operators.
+ */
+@Configuration
+public class MDCContextLifterConfiguration {
+
+    private final String mdcContextReactorKey = MDCContextLifterConfiguration.class.getName();
+
+    /**
+     * Default constructor for Spring configuration instantiation.
+     */
+    public MDCContextLifterConfiguration() {
+        super();
+    }
+
+    @PostConstruct
+    private void contextOperatorHook() {
+        Hooks.onEachOperator(
+                mdcContextReactorKey,
+                Operators.lift(
+                        (
+                         scannable,
+                         coreSubscriber
+                        ) -> new MDCContextLifter<>(coreSubscriber)
+                )
+        );
+    }
+
+    @PreDestroy
+    private void cleanupHook() {
+        Hooks.resetOnEachOperator(mdcContextReactorKey);
+    }
+}
